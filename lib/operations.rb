ac5e0f876bb7e10b5e1369abed336e9ebcd6d331
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-28 22:51:26 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
-def unsafe?(speed)
-
+require p
+ unsafe?(speed)
+  if 40 <= speed <= 60
+    'false'
+  else
+    'true'
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	(40 <= speed <= 60)? 'false' : 'true'
 end
 	
 
