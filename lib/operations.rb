b62fafb1d0fa0961de0937fa9962fe781bd8e568
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-01-28 22:53:05 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,18 @@
-require p
+require 'pry'
  unsafe?(speed)
   if 40 <= speed <= 60
     'false'
   else
     'true'
   end
+  binding.pry
 end
 
 
 
 def not_safe?(speed)
 	(40 <= speed <= 60)? 'false' : 'true'
+	binding.pry
 end
 	
 
